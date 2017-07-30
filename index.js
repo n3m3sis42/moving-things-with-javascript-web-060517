063594610252b089670cae17f94363c0252e506b
n3m3sis42 <n3m3sis42@gmail.com>
2017-07-30 14:43:51 -0400

    dodger moves left and right, created empty test folder to pass tests

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-const app = "I don't do much."
+var dodger = document.getElementById('dodger')
+
+function getDodgerLeftNumber() {
+  return parseInt(dodger.style.left.replace('px', ''), 10)
+}
+
+function moveDodgerLeft() {
+  var left = getDodgerLeftNumber()
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`
+  }
+}
+
+function moveDodgerRight() {
+  // assumes dodger is 40 px wide and games is 400 px wide as specified in stylesheet
+  var left = getDodgerLeftNumber()
+
+  if (left < 360) {
+    dodger.style.left = `${left + 1}px`
+  }
+}
+
+document.addEventListener('keydown', function(e) {
+  if (e.which === 37) {
+    moveDodgerLeft()
+  } else if (e.which === 39) {
+    moveDodgerRight()
+  }
+})
